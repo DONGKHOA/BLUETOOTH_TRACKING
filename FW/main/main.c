41b70f0e154c04fd2ba5d281131eb4ea6df73387
DONGKHOA <thanhkhoa2k3@gmail.com>
2025-01-03 22:34:33 +0700

    FW: upload project

--- FW/main/main.c
+++ FW/main/main.c
@@ -1,6 +1,40 @@
-#include <stdio.h>
+/******************************************************************************
+ *      INCLUDES
+ *****************************************************************************/
+
+#include <string.h>
+
+#include "nvs_rw.h"
+#include "app_mqtt_client.h"
+#include "app_data.h"
+
+/******************************************************************************
+ *    PRIVATE PROTOTYPE FUNCTION
+ *****************************************************************************/
+
+static inline void APP_MAIN_InitDataSystem(void)
+{
+  memset(s_data_system.u8_ssid, 0, sizeof(s_data_system.u8_ssid));
+  memset(s_data_system.u8_pass, 0, sizeof(s_data_system.u8_pass));
+
+  s_data_system.s_data_mqtt_queue = xQueueCreate(32, sizeof(uint32_t));
+}
+
+/******************************************************************************
+ *       MAIN FUNCTION
+ *****************************************************************************/
 
 void app_main(void)
 {
+  // BSP Initialization
+  NVS_Init();
 
+  // Main Initialization data system
+  APP_MAIN_InitDataSystem();
+
+  // App Initialization
+  APP_MQTT_CLIENT_Init();
+
+  // App Create Task
+  APP_MQTT_CLIENT_CreateTask();
 }
--- Test/ble_eddystone/main/esp_eddystone_demo.c
+++ Test/ble_eddystone/main/esp_eddystone_demo.c
@@ -15,6 +15,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <inttypes.h>
+#include <math.h>
 
 #include "esp_bt.h"
 #include "nvs_flash.h"
@@ -31,15 +32,12 @@
 #include "esp_eddystone_api.h"
 // #include "kalman_filter.h"
 
-// Định nghĩa cấu trúc Kalman
-typedef struct {
-    double Q_value;     // Độ nhiễu của quá trình (process noise covariance)
-    double Q_bias;      // Độ nhiễu bias (process noise covariance for bias)
-    double R_measure;   // Độ nhiễu của đo lường (measurement noise covariance)
-    double value;       // Giá trị RSSI được ước tính bởi bộ lọc Kalman
-    double bias;        // Bias được ước tính
-    double P[2][2];     // Ma trận hiệp phương sai
-} Kalman_t;
+  float _err_measure = 2;
+  float _err_estimate = 2;
+  float _q = 0.001;
+  float _current_estimate = 0;
+  float _last_estimate = 0;
+  float _kalman_gain = 0;
 
 static const char* DEMO_TAG = "EDDYSTONE_DEMO";
 
@@ -47,7 +45,7 @@ static const char* DEMO_TAG = "EDDYSTONE_DEMO";
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param);
 
 static esp_ble_scan_params_t ble_scan_params = {
-    .scan_type              = BLE_SCAN_TYPE_ACTIVE,
+    .scan_type              = BLE_SCAN_TYPE_PASSIVE,
     .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
     .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
     .scan_interval          = 0x50,
@@ -55,47 +53,13 @@ static esp_ble_scan_params_t ble_scan_params = {
     .scan_duplicate         = BLE_SCAN_DUPLICATE_DISABLE
 };
 
-Kalman_t Kalman_rssi = {
-    .Q_value = 0.00005f,
-    .Q_bias = 0.00005f,
-    .R_measure = 0.15f
-};
+double Kalman_GetRSSI(float mea) {
+  _kalman_gain = _err_estimate / (_err_estimate + _err_measure);
+  _current_estimate = _last_estimate + _kalman_gain * (mea - _last_estimate);
+  _err_estimate = (1.0f - _kalman_gain) * _err_estimate + fabsf(_last_estimate - _current_estimate) * _q;
+  _last_estimate = _current_estimate;
 
-double Kalman_GetRSSI(Kalman_t *Kalman, double newRSSI, double dt) {
-    // 1. Dự đoán (Prediction)
-    double rate = -Kalman->bias;       // RSSI không có bias (ở đây không sử dụng tốc độ thay đổi)
-    Kalman->value += dt * rate;        // Dự đoán giá trị tiếp theo
-
-    // Cập nhật ma trận hiệp phương sai
-    Kalman->P[0][0] += dt * (dt * Kalman->P[1][1] - Kalman->P[0][1] - Kalman->P[1][0] + Kalman->Q_value);
-    Kalman->P[0][1] -= dt * Kalman->P[1][1];
-    Kalman->P[1][0] -= dt * Kalman->P[1][1];
-    Kalman->P[1][1] += Kalman->Q_bias * dt;
-
-    // 2. Đo lường (Measurement Update)
-    double S = Kalman->P[0][0] + Kalman->R_measure; // Độ tin cậy của đo lường
-    double K[2];                                   // Kalman Gain
-    K[0] = Kalman->P[0][0] / S;
-    K[1] = Kalman->P[1][0] / S;
-
-    // Sai số đo lường (Innovation)
-    double y = newRSSI - Kalman->value;
-
-    // Cập nhật giá trị RSSI và bias
-    Kalman->value += K[0] * y;
-    Kalman->bias += K[1] * y;
-
-    // Cập nhật ma trận hiệp phương sai
-    double P00_temp = Kalman->P[0][0];
-    double P01_temp = Kalman->P[0][1];
-
-    Kalman->P[0][0] -= K[0] * P00_temp;
-    Kalman->P[0][1] -= K[0] * P01_temp;
-    Kalman->P[1][0] -= K[1] * P00_temp;
-    Kalman->P[1][1] -= K[1] * P01_temp;
-
-    // Trả về giá trị RSSI đã được làm mượt
-    return Kalman->value;
+  return _current_estimate;
 }
 
 static TimerHandle_t kalman_filter_timer;
@@ -148,7 +112,7 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* par
                         // esp_log_buffer_hex("EDDYSTONE_DEMO: Device address:", scan_result->scan_rst.bda, ESP_BD_ADDR_LEN);
 
                         // ESP_LOGI(DEMO_TAG, "RSSI RAW: %d dbm\n", scan_result->scan_rst.rssi);
-                        double smoothedRSSI  = Kalman_GetRSSI(&Kalman_rssi, scan_result->scan_rst.rssi, count/1000);
+                        float smoothedRSSI  = Kalman_GetRSSI(scan_result->scan_rst.rssi);
                         printf("%d,%.0f,%d\n",timer, smoothedRSSI, scan_result->scan_rst.rssi);
                         count = 0;
                         if (timer >= 300000)
