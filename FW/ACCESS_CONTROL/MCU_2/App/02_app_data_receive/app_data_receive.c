64cbec89575bc95a560d8a7fc2f5887a6bce1412
DONGKHOA <thanhkhoa2k3@gmail.com>
2025-04-04 22:27:55 +0700

    update

--- FW/ACCESS_CONTROL/MCU_2/App/02_app_data_receive/app_data_receive.c
+++ FW/ACCESS_CONTROL/MCU_2/App/02_app_data_receive/app_data_receive.c
@@ -88,6 +88,13 @@ APP_DATA_RECEIVE_task (void *arg)
 
     switch (s_receive_message.data[0])
     {
+      case DATA_SYNC_ENROLL_FINGERPRINT:
+
+        s_DATA_SYNC.u8_data_start     = s_receive_message.data[0];
+        s_DATA_SYNC.u8_data_packet[0] = s_receive_message.data[1];
+        s_DATA_SYNC.u8_data_length    = s_receive_message.data[2];
+        s_DATA_SYNC.u8_data_stop      = s_receive_message.data[3];
+        
       case DATA_SYNC_REQUEST_USER_DATA:
       case DATA_SYNC_ENROLL_FACE:
       case DATA_SYNC_REQUEST_AUTHENTICATION:
@@ -99,10 +106,6 @@ APP_DATA_RECEIVE_task (void *arg)
         s_DATA_SYNC.u8_data_stop      = s_receive_message.data[3];
 
         xQueueSend(*s_data_receive_data.p_data_mqtt_queue, &s_DATA_SYNC, 0);
-
-        break;
-
-      case DATA_SYNC_ENROLL_FINGERPRINT:
 
         break;
 
